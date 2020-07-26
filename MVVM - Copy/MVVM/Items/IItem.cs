03788df44d1a9cad38c250e1dc5c43ba8d6bfc6c
Chris Johnson <38737300+chrisjohnson7691@users.noreply.github.com>
2020-07-26 12:57:58 -0500

    vs removal
    removed vs file

---
+++ MVVM - Copy/MVVM/Items/IItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM.Items
+{
+    public interface IItem
+    {
+        string Name { get; set; }
+        DateTime TOC { get; set; }
+    }
+}
---
+++ MVVM - Copy/MVVM/Items/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM.Items
+{
+    public class Item : IItem
+    {
+        public Item(string addItemName)
+        {
+            Name = addItemName;
+            TOC = DateTime.Now;
+        }
+
+        public string Name { get; set; }
+        public DateTime TOC { get ; set ; }
+    }
+}
