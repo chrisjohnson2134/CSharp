2d8df0b5cea4b73ebc95dc92379529f84878c4dd
Chris Johnson <38737300+chrisjohnson7691@users.noreply.github.com>
2020-08-05 01:25:42 -0500

    Added Prism Example
    Working With Prism Framework; view Injection example.

---
+++ Prism/ViewInjection/ViewInjection/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using ModuleA;
+using Prism;
+using Prism.DryIoc;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+using ViewInjection.Views;
+
+namespace ViewInjection
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<ShellWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            moduleCatalog.AddModule<ModuleAModule>();
+        }
+    }
+}
