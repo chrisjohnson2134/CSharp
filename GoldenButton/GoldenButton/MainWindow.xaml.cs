0ae6fbbba97463866fc11f4595dbbe7b20dfc2d2
chrisjohnson2134 <cj106@evansville.edu>
2020-08-06 07:17:34 -0500

    Light Refactoring Gold Button
    Created Static Draw class that draw shapes onto grid. todo:modify draw function.

--- GoldenButton/GoldenButton/MainWindow.xaml.cs
+++ GoldenButton/GoldenButton/MainWindow.xaml.cs
@@ -1,20 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Media.Animation;
-using System.Threading;
-using System.Globalization;
+using GoldenButton.Drawing;
 
 namespace GoldenButton
 {
@@ -66,8 +57,6 @@ namespace GoldenButton
 
             if(youLostLabel.Visibility != Visibility.Visible && checkWin())
                 youWonLabel.Visibility = Visibility.Visible;
-
-
 
         }
 
@@ -184,44 +173,14 @@ namespace GoldenButton
             pBezierSegment.Points.Add(new Point(((start - end) * ((int)gridCanvas.Width / Convert.ToInt32(numSpacesTextBox.Text))), 0));
         }
 
-        private Line drawLine(double x1, double y1, double x2, double y2)
-        {
-            Line currLine = new Line();
-            currLine.Stroke = System.Windows.Media.Brushes.Black;
-            currLine.X1 = x1;
-            currLine.Y1 = y1;
-            currLine.X2 = x2;
-            currLine.Y2 = y2;
-            currLine.StrokeThickness = 1;
-            return currLine;
-        }
+        
 
-        private Ellipse drawBlueEllipse(double x, double y)
-        {
-            SolidColorBrush bluBrush = new SolidColorBrush(Color.FromRgb(0, 0, 255));
-            Ellipse ell1 = new Ellipse();
-            ell1.Height = (gridCanvas.Width / Convert.ToInt32(numSpacesTextBox.Text)) / 2;
-            ell1.Width = (gridCanvas.Width / Convert.ToInt32(numSpacesTextBox.Text)) / 2;
-            ell1.Fill = bluBrush;
-            ell1.Margin = new Thickness(x, y, 0, 0);//placement
-            return ell1;
-        }
-
-        private Ellipse drawGoldEllipse(double x, double y)
-        {
-            SolidColorBrush bluBrush = Brushes.Goldenrod;
-            Ellipse ell1 = new Ellipse();
-            ell1.Height = (gridCanvas.Width / Convert.ToInt32(numSpacesTextBox.Text)) / 2;
-            ell1.Width = (gridCanvas.Width / Convert.ToInt32(numSpacesTextBox.Text)) / 2;
-            ell1.Fill = bluBrush;
-            ell1.Margin = new Thickness(x, y, 0, 0);//placement
-            return ell1;
-        }
+        
 
         private void drawGrid(int spaces)
         {
-            gridCanvas.Children.Add(drawLine(5, 5, gridCanvas.Width - 2, 5));
-            gridCanvas.Children.Add(drawLine(5, gridCanvas.Height - 5, gridCanvas.Width - 2, gridCanvas.Height - 5));
+            gridCanvas.Children.Add(Draw.drawLine(5, 5, gridCanvas.Width - 2, 5));
+            gridCanvas.Children.Add(Draw.drawLine(5, gridCanvas.Height - 5, gridCanvas.Width - 2, gridCanvas.Height - 5));
             Console.WriteLine(spaces);
             for (int i = 0; i < spaces + 1; i++)
             {
@@ -232,20 +191,20 @@ namespace GoldenButton
 
                 Console.WriteLine(width);
 
-                gridCanvas.Children.Add(drawLine(width, 5, width, gridCanvas.Height - 5));
+                gridCanvas.Children.Add(Draw.drawLine(width, 5, width, gridCanvas.Height - 5));
             }
 
             for (int i = 0; i < spaces; i++)
             {
                 if (grid[i][0] == 'B')
                 {
-                    Ellipse created = drawBlueEllipse(i * (gridCanvas.Width / spaces) + (gridCanvas.Width / spaces) / 2, ((gridCanvas.Height) / 2) - (gridCanvas.Width / Convert.ToInt32(numSpacesTextBox.Text)) / 2/2);
+                    Ellipse created = Draw.drawBlueEllipse(gridCanvas,numSpacesTextBox.Text,i * (gridCanvas.Width / spaces) + (gridCanvas.Width / spaces) / 2, ((gridCanvas.Height) / 2) - (gridCanvas.Width / Convert.ToInt32(numSpacesTextBox.Text)) / 2/2);
                     listEllipse.Add(grid[i], created);
                     gridCanvas.Children.Add(created);//add to canvas
                 }
                 if (grid[i][0] == 'G')
                 {
-                    Ellipse created = drawGoldEllipse(i * (gridCanvas.Width / spaces) + (gridCanvas.Width / spaces) / 2, ((gridCanvas.Height) / 2) - (gridCanvas.Width / Convert.ToInt32(numSpacesTextBox.Text)) / 2/2);
+                    Ellipse created = Draw.drawGoldEllipse(gridCanvas, numSpacesTextBox.Text,i * (gridCanvas.Width / spaces) + (gridCanvas.Width / spaces) / 2, ((gridCanvas.Height) / 2) - (gridCanvas.Width / Convert.ToInt32(numSpacesTextBox.Text)) / 2/2);
                     listEllipse.Add(grid[i], created);
                     gridCanvas.Children.Add(created);//add to canvas
                 }
