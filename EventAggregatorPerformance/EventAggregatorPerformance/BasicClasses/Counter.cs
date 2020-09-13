93116fae71af487746d9422510079bcea69ce8dd
chrisjohnson2134 <cj106@evansville.edu>
2020-09-13 18:22:54 -0500

    Event Aggregator Project Added
    Added Project for testing the Event Aggregator vs. Traditional C# events.

---
+++ EventAggregatorPerformance/EventAggregatorPerformance/BasicClasses/Counter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Events;
+
+namespace EventAggregatorPerformance.BasicClasses
+{
+    public class Counter : PubSubEvent<int>
+    {
+        public event EventHandler ThresholdReached;
+
+        public int threshold;
+        public int Total;
+
+        public Counter()
+        {
+            threshold = 0;
+            Total = 0;
+        }
+
+        public void Add(int x)
+        {
+            Total += x;
+            if (Total >= threshold)
+            {
+                OnThresholdReached(EventArgs.Empty);
+            }
+        }
+
+        protected virtual void OnThresholdReached(EventArgs e)
+        {
+            EventHandler handler = ThresholdReached;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        
+    }
+}
